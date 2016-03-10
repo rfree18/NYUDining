847a3e71b61b2eef72843c8b06ee5de17702fca3
Ross Freeman <rfreeman1@me.com>
2016-03-10 10:22:43 -0500

    Change database

---
+++ NYUDining/RFParseConfig.h
@@ -0,0 +1,22 @@
+//
+//  RFParseConfig.h
+//  NYUDining
+//
+//  Created by Ross Freeman on 3/10/16.
+//  Copyright © 2016 Ross Freeman. All rights reserved.
+//
+
+#ifndef RFParseConfig_h
+#define RFParseConfig_h
+
+extern NSString *const appId;
+extern NSString *const masterKey;
+extern NSString *const clientKey;
+extern NSString *const serverUrl;
+
+//#define kAppId = @"8VAOyRguvMpTYDZJk91HCf90dKs7UarU79p7s2l0"
+//#define kMasterKey = @"AGI49NgIbIQE33W5NOt3QmKakRpD2s6GEJc54Cgj"
+//#define kClientKey = @"AmaAAD3a4EemgfH8ftspXjLH7Qn1x0loUUir2bra"
+//#define kServerUrl = @"parseserver-6c297-env.us-east-1.elasticbeanstalk.com/parse"
+
+#endif /* RFParseConfig_h */
---
+++ NYUDining/RFParseConfig.m
@@ -0,0 +1,15 @@
+//
+//  RFParseConfig.m
+//  NYUDining
+//
+//  Created by Ross Freeman on 3/10/16.
+//  Copyright © 2016 Ross Freeman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RFParseConfig.h"
+
+NSString *const appId = @"8VAOyRguvMpTYDZJk91HCf90dKs7UarU79p7s2l0";
+NSString *const masterKey = @"AGI49NgIbIQE33W5NOt3QmKakRpD2s6GEJc54Cgj";
+NSString *const clientKey = @"AmaAAD3a4EemgfH8ftspXjLH7Qn1x0loUUir2bra";
+NSString *const serverUrl = @"http://parseserver-6c297-env.us-east-1.elasticbeanstalk.com/parse";
